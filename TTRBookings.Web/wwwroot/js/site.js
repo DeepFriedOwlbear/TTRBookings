9de7dc6f01e7a5b641cafea159f51eb6493c84a0
DeepFriedOwlbear <lukas2504@gmx.at>
2021-04-14 16:47:02 +0200

    RemoveFromUI added to Bookings.Index; HandleFormSubmit moved to site.js; Some layout changes;

--- TTRBookings.Web/wwwroot/js/site.js
+++ TTRBookings.Web/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// Event handler for a form submit event.
+async function handleFormSubmit(event, callback) {
+    event.preventDefault();
+
+    // This gets the element which the event handler was attached to.
+    const form = event.currentTarget;
+
+    // This takes the API URL from the form's `action` attribute.
+    const url = form.action;
+
+    try {
+        // This takes all the fields in the form and makes their values
+        // available through a `FormData` instance.
+        const formData = new FormData(form);
+
+        const responseData = await fetch(url,
+            {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(Object.fromEntries(formData)),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                //Use Callback function and give it the success state + submitted formData
+                callback(data.success, formData);
+                //return data;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    } catch (error) {
+        console.error(error);
+    }
+}
