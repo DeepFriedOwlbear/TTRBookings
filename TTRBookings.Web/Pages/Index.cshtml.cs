ce622b43c633a77309be4ef3543f8437cc840df3
DeepFriedOwlbear <lukas2504@gmx.at>
2021-05-21 12:27:25 +0200

    Flatpickr for DateTime inputs implemented;

--- TTRBookings.Web/Pages/Index.cshtml.cs
+++ TTRBookings.Web/Pages/Index.cshtml.cs
@@ -19,10 +19,6 @@ namespace TTRBookings.Web.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly IRepository repository;
 
-        [BindProperty] public string HouseId { get; set; }
-
-        public List<SelectListItem> Houses { get; } = new List<SelectListItem> { };
-
         public IndexModel(IRepository repository)
         {
             this.repository = repository;
@@ -30,34 +26,7 @@ namespace TTRBookings.Web.Pages
 
         public void OnGet()
         {
-            if (string.IsNullOrEmpty(HttpContext.Session.GetString("HouseId")))
-            {
-                Houses.AddRange(SelectListHelper.PopulateList(
-                    repository.List<House>(), e => e.Name
-                    ));
 
-                HttpContext.Session.SetString("HouseId", Houses.FirstOrDefault().Value);
-            }
-            else
-            {
-                Houses.AddRange(SelectListHelper.PopulateList(
-                    repository.List<House>(), e => e.Name, 
-                    Guid.Parse(HttpContext.Session.GetString("HouseId"))
-                    ));
-            }
         }
-
-        public IActionResult OnPost()
-        {
-            Houses.AddRange(SelectListHelper.PopulateList(
-                repository.List<House>(), e => e.Name,
-                Guid.Parse(HouseId)
-                ));
-
-            HttpContext.Session.SetString("HouseId", HouseId);
-
-            return Page();
-        }
-
     }
 }
